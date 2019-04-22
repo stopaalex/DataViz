5680f8286f66da242a6db58aad0577d248101f04
Alex Stopa <stopa.alex@gmail.com>
2019-04-22 09:14:15 -0400

    UPDATE ::: npm scripts

--- dev/api/authorize.py
+++ dev/api/authorize.py
@@ -16,12 +16,15 @@ Finish importing all required python dependencies
 
 
 def authUser():
+    returnText = ''
     if os.path.exists('/db/users.table.csv'):
+        returnText = 'there is a table'
         print('there is a table')
         # read in the csv to see if we need to reauth them
     else:
-        print('new user')
+        returnText = 'there is no table, create table and navigate to create login page'
+        print('there is no table, create table')
         print('navigate to login/user')
         # create the auth table with the users creds
 
-    return 'hit the authorize function'
+    return returnText
